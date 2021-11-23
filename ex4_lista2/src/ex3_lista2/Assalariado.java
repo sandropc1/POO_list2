a070cd15991210c579bb2c094674455df15f10bc
sandropc1 <92626531+sandropc1@users.noreply.github.com>
2021-11-23 15:42:29 -0300

    Add files via upload

---
+++ ex4_lista2/src/ex3_lista2/Assalariado.java
@@ -0,0 +1,17 @@
+package ex3_lista2;
+
+public abstract class Assalariado extends Empregado {
+	
+    protected float salarioSemanal;
+	
+	public Assalariado() {
+		
+	}
+
+	public void setSalarioSemanal(float salarioSemanal) {
+		
+		this.salarioSemanal = salarioSemanal;
+		
+	}
+
+}
